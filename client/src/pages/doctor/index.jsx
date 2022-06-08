875c0d5ae87f42975ba412f41fa10976dc3f1df4
Sarfraz <alamsarfraz422@gmail.com>
2022-06-08 23:47:07 +0530

    hm

--- client/src/pages/doctor/index.jsx
+++ client/src/pages/doctor/index.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Tabs, Divider } from "antd";
+import React, { useCallback, useEffect } from "react";
+import { Tabs, Divider, message } from "antd";
 
 import Header from "../../components/Header";
 import Appointments from "./modules/Appointments";
@@ -7,28 +7,76 @@ import Patients from "./modules/patients";
 import Notifications from "./modules/notifications";
 import { authState } from "../../atoms/auth";
 import { useRecoilValue } from "recoil";
+import { socket } from "../../api/socket";
+import moment from "moment";
+import { instance } from "../../api/instance";
+
+export const DoctorContext = React.createContext();
 
 const Doctor = () => {
   const [online, setOnline] = React.useState(true);
+  const [AppointmentsData, setAppointmentsData] = React.useState([]);
   const Auth = useRecoilValue(authState);
   const user = { ...Auth.user, online };
-  return (
-    <div style={{ padding: "20px" }}>
-      <Header title="Doctor" subTitle="" user={user} />
-      <Divider />
 
-      <Tabs defaultActiveKey="1">
-        <Tabs.TabPane tab="Appointments" key="1">
-          <Appointments />
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="Patients" key="3">
-          <Patients />
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="Notifications" key="4">
-          <Notifications />
-        </Tabs.TabPane>
-      </Tabs>
-    </div>
+  const getAppointments = useCallback(async () => {
+    const res = await instance.get(`/doctor/get-appointments`);
+    console.log(res.data);
+    setAppointmentsData(() => {
+      return res.data.appointments.map((appointment) => ({
+        ...appointment,
+
+        date: moment(appointment.date).format("MMMM Do YYYY, h:mm:ss a"),
+        patientname: appointment.Patient.name,
+      }));
+    });
+  }, []);
+
+  useEffect(() => {
+    getAppointments();
+  }, []);
+
+  useEffect(() => {
+    socket.on("new-appointment-created", (data) => {
+      if (data.doctor.AuthId === Auth.user.id) {
+        console.log(data);
+        setAppointmentsData((prev) => [
+          {
+            date: moment(data.Appointment.date).format(
+              "MMMM Do YYYY, h:mm:ss a"
+            ),
+            patientname: data.patient.name,
+            id: data.id,
+          },
+          ...prev,
+        ]);
+      }
+    });
+
+    return () => {
+      socket.off("new-appointment-created");
+    };
+  }, []);
+
+  return (
+    <DoctorContext.Provider value={{ user, AppointmentsData }}>
+      <div style={{ padding: "20px" }}>
+        <Header title="Doctor" subTitle="" user={user.DOCTOR} />
+        <Divider />
+
+        <Tabs defaultActiveKey="1">
+          <Tabs.TabPane tab="Appointments" key="1">
+            <Appointments />
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Patients" key="3">
+            <Patients />
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Notifications" key="4">
+            <Notifications />
+          </Tabs.TabPane>
+        </Tabs>
+      </div>
+    </DoctorContext.Provider>
   );
 };
 
